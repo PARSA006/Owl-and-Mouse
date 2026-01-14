ffab556c8f05482e57266b0e175fef5fcdcad377
Liibic <leo-harju@outlook.com>
2026-01-14 13:59:58 +0200

    moving

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform[] patrolPoints;
+
+    [Header("Settings")]
+    [SerializeField] private float patrolWaitTime = 2f;
+    [SerializeField] private float stopAtDistance = 0.5f;
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    private int _currentPatrolIndex;
+    private bool _isWaiting;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        GoToNextPatrolPoint();
+    }
+
+    private void Update()
+    {
+        Patrol();
+        UpdateAnimations();
+    }
+
+    private void Patrol()
+    {
+        if (_isWaiting) return;
+
+        if (!_agent.pathPending && _agent.remainingDistance <= stopAtDistance)
+        {
+            StartCoroutine(WaitAtPatrolPoint());
+        }
+    }
+
+    private IEnumerator WaitAtPatrolPoint()
+    {
+        _isWaiting = true;
+        _agent.isStopped = true;
+
+        yield return new WaitForSeconds(patrolWaitTime);
+
+        _agent.isStopped = false;
+        GoToNextPatrolPoint();
+        _isWaiting = false;
+    }
+
+    private void GoToNextPatrolPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+
+        _agent.SetDestination(patrolPoints[_currentPatrolIndex].position);
+        _currentPatrolIndex = (_currentPatrolIndex + 1) % patrolPoints.Length;
+    }
+
+    private void UpdateAnimations()
+    {
+        bool isWalking = _agent.velocity.sqrMagnitude > 0.01f;
+        _animator.SetBool("IsWalking", isWalking);
+    }
+}
